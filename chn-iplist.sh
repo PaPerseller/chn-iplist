f0987fd55b99f424b0ad782b4e9a4ec2701b567f
PaPerseller <2445513240@qq.com>
2018-02-04 16:06:26 +0800

    Update chn-iplist.sh

--- chn-iplist.sh
+++ chn-iplist.sh
@@ -2,6 +2,6 @@ et -e -o pipefail
  
 wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | \
     awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > \
-    /tmp/chinadns_chnroute.txt
+    /tmp/chn-iplist.txt
  
-mv /tmp/chinadns_chnroute.txt /etc/storage/DEADC0DE/chnroute/chnroute.txt
+mv /tmp/chn-iplist.txt /etc/storage/DEADC0DE/chnroute/chnroute.txt
