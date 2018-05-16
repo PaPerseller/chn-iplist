695e529b84a61c213313b8933ba3fadb5ddd7846
PaPerseller <2445513240@qq.com>
2018-05-16 10:33:23 +0800

    Update Shadowsocket.conf

--- chn-iplist.sh
+++ chn-iplist.sh
@@ -1,4 +1,4 @@
-et -e -o pipefail
+set -e -o pipefail
  
 wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | \
     awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > \
