64de71934ec00b4ca1a81779c4b7ce74ecbe6f5b
PaPerseller <huseraph@gmail.com>
2023-03-22 14:14:54 +0800

    update script

--- scripts/v6ipcidr.py
+++ scripts/v6ipcidr.py
@@ -1,17 +1,21 @@
-# 列出每个行的文本
+before_text = 'IP-CIDR,'
+after_text = ',no-resolve'
+
 with open("./chnroute-ipv6.txt", "r") as input_file:
     lines = input_file.readlines()
 
-# 定义要添加的文本
-before_text = "IP-CIDR,"
-after_text = ",no-resolve"
-
-# 将特定内容添加到每行文本前后
 new_lines = []
 for line in lines:
     new_line = before_text + line.strip() + after_text + "\n"
     new_lines.append(new_line)
 
-# 将修改后的文本写回文件
 with open("./ipv6.list", "w") as output_file:
     output_file.writelines(new_lines)
+with open("./ipv6.list", "r") as output_file:
+    outlines = output_file.readlines()
+
+header = ['# 适用于 shadowrocket 等 ipv6 规则前缀为 IP-CIDR 的应用\n']
+header.extend(outlines)
+
+with open("./ipv6.list", "w") as output_file:
+    output_file.writelines(header)
