4e8681e1853bcb256d9a507c8522433004e58302
PaPerseller <huseraph@gmail.com>
2023-11-17 13:37:59 +0800

    update

--- scripts/update_chnip.py
+++ scripts/update_chnip.py
@@ -6,12 +6,10 @@ import math
 #update chnroute-ipv6
 logger = logging.getLogger(__name__)
 
-
 def update_ip():
     url = 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
     timeout = 30
     save_to_file6 = './chnroute-ipv6.txt'
-
 
     logger.info(f'connecting to {url}')
 
@@ -41,33 +39,63 @@ def update_ip():
     with open(save_to_file6, 'wt') as f:
         f.writelines([f'{x}\n' for x in ipNetwork_list6])
 
-    logger.info('all done')
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     update_ip()
 
+#update chnroute-ipv4
+url01 = "https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
 
-#update chroute-ipv4
-url = "https://raw.githubusercontent.com/PaPerseller/someip/build/cidr.txt"
-
-response = requests.get(url)
+response = requests.get(url01)
 
 if response.status_code == 200:
-    with open("chnroute-ipv4.txt", "wb") as file:
-        file.write(response.content)
+    with open("./china_ip_list.txt", "wb") as a_file:
+        a_file.write(response.content)
 else:
     print(f"文件下载失败，状态码: {response.status_code}")
 
-#update chnroute
-with open("./chnroute-ipv4.txt", 'r') as a_file:
+url02 = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt"
+
+response = requests.get(url02)
+
+if response.status_code == 200:
+    with open("./china.txt", "wb") as b_file:
+        b_file.write(response.content)
+else:
+    print(f"文件下载失败，状态码: {response.status_code}")
+
+with open("./china_ip_list.txt", 'r') as a_file:
     a_content = a_file.read()
 
-with open("./chnroute-ipv6.txt", 'r') as b_file:
+with open("./china.txt", 'r') as b_file:
     b_content = b_file.read()
 
 merged_content = a_content + b_content
 
-with open('./chnroute.txt', 'w') as output_file:
-        output_file.write(merged_content)
+with open('./chnroute-ipv4.txt', 'w') as c_file:
+    c_file.write(merged_content)
+
+with open('./chnroute-ipv4.txt', 'r') as c_file:
+    c_lines = c_file.readlines()
+
+    ipNetwork_list4 = c_lines
+
+    logger.info('merging')
+    ipNetwork_list4 = netaddr.cidr_merge(ipNetwork_list4)
+    logger.info('writing to file')
+
+with open("./chnroute-ipv4.txt", 'wt') as d_file:
+    for ip_network in ipNetwork_list4:
+        d_file.write(str(ip_network) + "\n")
+
+#merge v4 & v6
+with open("./chnroute-ipv4.txt", 'r') as d_file:
+    d_content = d_file.read()
+
+with open('./chnroute-ipv6.txt', 'r') as e_file:
+    e_content = e_file.read()
+
+merged_content = a_content + b_content
+
+with open('./chnroute.txt', 'w') as f_file:
+    f_file.write(merged_content)
