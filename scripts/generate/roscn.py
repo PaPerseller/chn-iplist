432959490604f4d0e9806ce53ccfb5af79400111
PaPerseller <huseraph@gmail.com>
2024-04-01 22:33:57 +0800

    exclude fakeip address range

--- scripts/generate/roscn.py
+++ scripts/generate/roscn.py
@@ -14,7 +14,6 @@ def format_cidr(file_path, output_path):
         f.write("add address=192.0.2.0/24 list=CN comment=private-network\n")
         f.write("add address=192.88.99.0/24 list=CN comment=private-network\n")
         f.write("add address=192.168.0.0/16 list=CN comment=private-network\n")
-        f.write("add address=198.18.0.0/15 list=CN comment=private-network\n")
         f.write("add address=198.51.100.0/24 list=CN comment=private-network\n")
         f.write("add address=203.0.113.0/24 list=CN comment=private-network\n")
         f.write("add address=224.0.0.0/3 list=CN comment=private-network\n")
