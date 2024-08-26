5cbf143e7a70ef3cc44b46449660bce261ab4d86
PaPerseller <huseraph@gmail.com>
2024-08-26 23:19:11 +0800

    optimize code

--- scripts/update_chnip.py
+++ scripts/update_chnip.py
@@ -1,53 +1,45 @@
 import netaddr
 import requests
-import os
 
-def download_from_url(url, file_path):
-    with open(file_path, "wb") as file:
-        file.write(requests.get(url).content)
-
-def parse_and_merge_ip(url, save_to_file):
+def parse_and_merge_ip(url):
     print(f'Connecting to {url}...')
-
     ipNetwork_list = []
-
     lines = requests.get(url).text.splitlines()
     for line in lines:
         if '|CN|ipv6|' in line:
             elems = line.split('|')
             ip_start = elems[3]
             cidr_prefix_length = elems[4]
-            ipNetwork_list.append(netaddr.IPNetwork(f'{ip_start}/{cidr_prefix_length}\n'))
+            ipNetwork_list.append(netaddr.IPNetwork(f'{ip_start}/{cidr_prefix_length}'))
+    return netaddr.cidr_merge(ipNetwork_list)
 
-    ipNetwork_list = netaddr.cidr_merge(ipNetwork_list)
+def download_and_parse(url):
+    print(f'Downloading from {url}...')
+    return [netaddr.IPNetwork(line.strip()) for line in requests.get(url).text.splitlines()]
 
-    with open(save_to_file, 'wt') as f:
-        f.writelines([f'{x}\n' for x in ipNetwork_list])
+def merge_and_sort_networks(networks1, networks2):
+    print('Merging and sorting networks...')
+    return netaddr.cidr_merge(networks1 + networks2)
 
-def merge_and_sort_files(file1, file2, output):
-    print('Merging and sorting files...')
-    with open(file1, 'r') as f1, open(file2, 'r') as f2:
-        ipNetwork_list = f1.readlines() + f2.readlines()
-        
-    ipNetwork_list = netaddr.cidr_merge([netaddr.IPNetwork(x) for x in ipNetwork_list])
-    with open(output, 'wt') as f:
-        for ip_network in ipNetwork_list:
-            f.write(str(ip_network) + "\n")
-
-def cleanup(files):
-    for file in files:
-        if os.path.exists(file):
-            os.remove(file)
-            print(f"Deleted file: {file}")
+def write_to_file(networks, filename):
+    print(f'Writing to {filename}...')
+    with open(filename, 'wt') as f:
+        for network in networks:
+            f.write(str(network) + "\n")
 
 if __name__ == '__main__':
-    parse_and_merge_ip('https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest', './chnroute-ipv6.txt')
+    # Parse and merge IPv6 networks
+    ipv6_networks = parse_and_merge_ip('https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest')
+    write_to_file(ipv6_networks, './chnroute-ipv6.txt')
 
-    download_from_url("https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt", "./china_ip_list.txt")
-    download_from_url("https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt", "./china.txt")
+    # Download and parse IPv4 networks
+    china_ip_list = download_and_parse("https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt")
+    china_txt = download_and_parse("https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt")
 
-    merge_and_sort_files("./china_ip_list.txt", "./china.txt", "./chnroute-ipv4.txt")
-    merge_and_sort_files("./chnroute-ipv4.txt", './chnroute-ipv6.txt', './chnroute.txt')
+    # Merge and sort IPv4 networks
+    ipv4_networks = merge_and_sort_networks(china_ip_list, china_txt)
+    write_to_file(ipv4_networks, './chnroute-ipv4.txt')
 
-    # Clean up downloaded files
-    cleanup(["./china_ip_list.txt", "./china.txt"])
+    # Merge IPv4 and IPv6 networks
+    all_networks = merge_and_sort_networks(ipv4_networks, ipv6_networks)
+    write_to_file(all_networks, './chnroute.txt')
